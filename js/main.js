d5cd02782e692a74d1fe5d0d57687d8ca012db04
Xiomara Sánchez <40286327+xjsanchezf@users.noreply.github.com>
2018-11-26 10:57:44 -0500

    Added 'main.js' file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-new commit :)
+// menu-scroll
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        document.getElementById("menu-scroll").style.display = "block";
+    } else {
+        document.getElementById("menu-scroll").style.display = "none";
+    }
+}
